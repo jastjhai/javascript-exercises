349a9dffdf33c424e71f4e846400a4495205df11
jastjhai <jaslyntjhai@gmail.com>
2025-10-28 16:18:27 +0800

    Completed JavaScript exercises 01-05

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,9 +1,11 @@
 function numberChecker(number) {
-  if (number === 6) {
+  
+  if (number >= 10) {
     return true;
   } else {
     return false;
-  }
+  } 
+  
 }
 
 // Do not edit below this line
